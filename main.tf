225f7ec92a82840e7b2284b938ec9d884405899d
amir4545 <73450541+amir4545@users.noreply.github.com>
2025-07-06 03:17:46 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region     = "us-east-1"
+  #access_key = ""
+  #secret_key = ""
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-05ffe3c48a9991133"
+  instance_type = "t3.micro"
+}
