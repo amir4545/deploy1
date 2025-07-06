d1ab9542f7f7ed73046e22a002ac28ebf217f7d8
amir4545 <73450541+amir4545@users.noreply.github.com>
2025-07-06 04:19:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,30 +5,19 @@ provider "aws" {
 }
 
 
-# Get the default VPC
-data "aws_vpc" "default" {
-  default = true
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-# Get all subnets in the default VPC
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-resource "aws_instance" "web" {
-  ami                    = "ami-05ffe3c48a9991133"
-  instance_type          = "t3.micro"
-  subnet_id              = data.aws_subnets.default.ids[0]
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
 }
 
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = data.aws_vpc.default.id
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 22
@@ -43,4 +32,11 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-05ffe3c48a9991133"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 }
