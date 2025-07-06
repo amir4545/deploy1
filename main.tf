bb44a4f0d6c0b591dcdece124f47064a83c735f6
amir4545 <73450541+amir4545@users.noreply.github.com>
2025-07-06 03:59:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,29 +4,42 @@ provider "aws" {
   #secret_key = ""
 }
 
-# Get default VPC
+# Get the default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-# Get a subnet in the default VPC
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+# Get all subnets in the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-05ffe3c48a9991133"
-  instance_type = "t3.micro"
-  subnet_id     = data.aws_subnet_ids.default.ids[0]
-  
-  # Use security group explicitly if needed
-  # vpc_security_group_ids = ["sg-xxxx"]
+  ami                    = "ami-05ffe3c48a9991133"
+  instance_type          = "t3.micro"
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 }
 
-#resource "aws_instance" "web" {
-#  ami           = "ami-05ffe3c48a9991133"
-#  instance_type = "t3.micro"
-#}
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
