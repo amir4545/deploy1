35329a5b2f1d5114ccf4da480171390b09d4ce95
amir4545 <73450541+amir4545@users.noreply.github.com>
2025-07-06 03:54:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,29 @@ provider "aws" {
   #secret_key = ""
 }
 
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get a subnet in the default VPC
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-05ffe3c48a9991133"
+  instance_type = "t3.micro"
+  subnet_id     = data.aws_subnet_ids.default.ids[0]
+  
+  # Use security group explicitly if needed
+  # vpc_security_group_ids = ["sg-xxxx"]
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-05ffe3c48a9991133"
   instance_type = "t3.micro"
 }
+
+
 
